d5d938f83c8c41c33ce27f87bcce22348e51ab3a
Raav Koper <rkoper@Raavs-MacBook-Pro.local>
2023-01-25 16:09:56 +0100

    adede

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -18,9 +18,12 @@ int main() {
 	// jonna.executeForm(pressy);
 
 	RobotomyRequestForm robby("Terminator");
-
+	try {
+		bammi.executeForm(robby);
+	} catch (std::exception &e) {
+		std::cerr << e.what() << std::endl;
+	}
 	bammi.signForm(robby);
-	bammi.executeForm(robby);
 	std::cout << std::endl;
 	bammi.executeForm(robby);
 	std::cout << std::endl;
